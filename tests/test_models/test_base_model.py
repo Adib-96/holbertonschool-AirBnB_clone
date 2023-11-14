8dc426a1b4a0d0e7fbbab075bc19d141233de8d9
Adib-96 <adibbh1996@gmail.com>
2023-11-14 22:35:22 +0100

    fix pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,20 +4,19 @@ import unittest
 mod_doc = base_model.__doc__
 BaseModel = base_model.BaseModel
 
+
 class TestBaseModelDocs(unittest.TestCase):
     """Tests to check the documentation and style of BaseModel class"""
     def test_module_docstring(self):
         """Test for module docstring"""
-        self.assertIsNot(mod_doc,None,"base_model.py needs a docstring")
+        self.assertIsNot(mod_doc, None, "base_model.py needs a docstring")
         self.assertTrue(len(mod_doc) > 1,
                         "base_model.py needs a docstring")
-    
 
-    
-    
+
 class TestBaseModel(unittest.TestCase):
     """Test the baseModel class"""
     def test_instantiation(self):
         """Check if the Object is correctly instantiated"""
         obj = BaseModel()
-        self.assertIs(type(obj),BaseModel,msg="Object is correctly instantiated")
+        self.assertIs(type(obj), BaseModel)
