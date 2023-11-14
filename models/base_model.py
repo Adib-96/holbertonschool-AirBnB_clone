e8a90ea43b7b940393955d51e595f1cdb4a51ea8
Adib-96 <adibbh1996@gmail.com>
2023-11-14 23:04:29 +0100

    implement isoformat to a to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,12 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
     def save(self):
         """Update the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
