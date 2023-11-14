be873503931690ea1710176820ab9e96956fa808
Adib-96 <adibbh1996@gmail.com>
2023-11-14 22:34:06 +0100

    BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,15 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        return (f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>")
+        return (f"[<{self.__class__.__name__}>] (<{self.id}>) <{self.__dict__}>")
 
     def save(self):
         """Update the public instance attribute with the current datetime"""
-        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns the __dict__ representation of an instance"""
