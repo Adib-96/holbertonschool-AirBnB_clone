81328c14856fefa31d3790d4dc88390d47cbef8d
Adib-96 <adibbh1996@gmail.com>
2023-11-14 22:59:59 +0100

    implement isoformat to a to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,6 @@ class BaseModel:
         """returns the __dict__ representation of an instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        new_dict['created_at'] = new_dict['created_at'].isoformat()
-        new_dict['updated_at'] = new_dict['updated_at'].isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
