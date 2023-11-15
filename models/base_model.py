44989b62352f0e3c58d526315ed19871129b94bc
Adib-96 <adibbh1996@gmail.com>
2023-11-15 22:00:14 +0100

    test b

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class BaseModel:
     """this is the class BaseModel"""
 
-    def __init__(self):
+    def __init__(self,*args,**kwargs):
         """Constructor function"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
