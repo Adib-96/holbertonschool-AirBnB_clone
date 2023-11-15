6bbde6e4fb0d17bff468f1ee57f64b2df8bc1ec0
Adib-96 <adibbh1996@gmail.com>
2023-11-15 16:39:26 +0100

    fix bug

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        
     def __str__(self):
         """String representation of the BaseModel class"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
         """Update the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
