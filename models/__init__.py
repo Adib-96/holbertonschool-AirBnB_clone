5cfe8c3e57fc6846637292c1545316854698581a
Adib-96 <adibbh1996@gmail.com>
2023-11-19 17:04:11 +0100

    Fix logic error

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""the __init__ method for the initialization of an instance"""
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,46 @@
-#!/usr/bin/python3
-"""
-BaseModel that defines all common attributes/methods for other classes.
-"""
-import uuid
+from uuid import uuid4
+import models
 from datetime import datetime
-from models import storage
 
 
-class BaseModel:
-    """this is the class BaseModel"""
+class BaseModel():
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Constructor function"""
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == 'created_at' or k == 'updated_at':
+                        v = datetime.datetime.strptime(
+                            v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, k, v)
+
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def __str__(self):
         """return string representation"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        clnmae = self.__class__.__name__
+        return "[{}] ({}) {}".format(clnmae, self.id, self.__dict__)
 
     def save(self):
-        """Update the public instance attribute with the current datetime"""
-        self.updated_at = datetime.now()
-        storage.save()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing all
-        keys/values of __dict__ of the instance"""
+        """Return a dict representation of an instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
+
         return new_dict
