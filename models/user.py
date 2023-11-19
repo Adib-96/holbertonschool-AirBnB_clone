f0f598924ffde48794045fa647bb9e02256fc56c
Adib-96 <adibbh1996@gmail.com>
2023-11-19 18:35:22 +0100

    testing files

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Represent a User model
-    
     Class_Attributes:
         email: User email
         password: user password
